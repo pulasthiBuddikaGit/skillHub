2009a702d7396ae3c9ce8e26410fc542642c0022
Nisalka Jayasinghe <jayasinghenisalka@gmail.com>
2025-05-08 16:31:10 +0530

    Update ProgressService.java

--- backend/src/main/java/com/paf/socialmedia/service/progress/ProgressService.java
+++ backend/src/main/java/com/paf/socialmedia/service/progress/ProgressService.java
@@ -22,172 +22,172 @@ import java.util.Optional;
 @Service
 public class ProgressService {
     @Autowired
-    private ProgressRepository workoutRepository;
+    private ProgressRepository progressRepository;
 
     @Autowired
-    private ProgressCommentRepository workoutCommentRepository;
+    private ProgressCommentRepository progressCommentRepository;
 
     @Autowired
     private UserRepository userRepository;
 
-    public ResponseEntity<?> getWorkoutById(String id){
-        Optional<Progress> workout =  workoutRepository.findById(id);
-        if(workout.isPresent()){
-            return new ResponseEntity<>(workout.get(), HttpStatus.OK);
+    public ResponseEntity<?> getProgressById(String id){
+        Optional<Progress> progress =  progressRepository.findById(id);
+        if(progress.isPresent()){
+            return new ResponseEntity<>(progress.get(), HttpStatus.OK);
         }else{
             return new ResponseEntity<>("No Progress Found",HttpStatus.NOT_FOUND);
         }
     }
-    public ResponseEntity<?> getWorkouts(){
-        List<Progress> workouts = workoutRepository.findAll();
+    public ResponseEntity<?> getProgresses(){
+        List<Progress> progresses = progressRepository.findAll();
 
-        List<ProgressDTO> workoutDTOList = new ArrayList<>();
+        List<ProgressDTO> progressDTOList = new ArrayList<>();
 
-        for (Progress workout:workouts) {
-            ProgressDTO workoutDTO = new ProgressDTO();
-            workoutDTO.setId(workout.getId());
-            workoutDTO.setCaption(workout.getCaption());
-            workoutDTO.setImgLink(workout.getImgLink());
-            workoutDTO.setUpdatedAt(workout.getUpdatedAt());
-            workoutDTO.setCreatedAt(workout.getCreatedAt());
-            workoutDTO.setLikedby(workout.getLikedby());
-            workoutDTO.setUserId(workout.getUserId());
+        for (Progress progress:progresses) {
+            ProgressDTO progressDTO = new ProgressDTO();
+            progressDTO.setId(progress.getId());
+            progressDTO.setCaption(progress.getCaption());
+            progressDTO.setImgLink(progress.getImgLink());
+            progressDTO.setUpdatedAt(progress.getUpdatedAt());
+            progressDTO.setCreatedAt(progress.getCreatedAt());
+            progressDTO.setLikedby(progress.getLikedby());
+            progressDTO.setUserId(progress.getUserId());
 
-            Optional<User> user =  userRepository.findById(workout.getUserId());
+            Optional<User> user =  userRepository.findById(progress.getUserId());
             if(user.isPresent()) {
-                workoutDTO.setUsername(user.get().getUsername());
-                workoutDTO.setProfileImage(user.get().getProfileImage());
+                progressDTO.setUsername(user.get().getUsername());
+                progressDTO.setProfileImage(user.get().getProfileImage());
             }
 
-            List<ProgressComment> workoutComments = workoutCommentRepository.findByWorkoutId(workout.getId());
-            if(workoutComments.size() > 0){
-                List<ProgressCommentDTO> workoutCommentDTOList = new ArrayList<>();
+            List<ProgressComment> progressComments = progressCommentRepository.findByProgressId(progress.getId());
+            if(progressComments.size() > 0){
+                List<ProgressCommentDTO> progressCommentDTOList = new ArrayList<>();
 
-                for(ProgressComment workoutComment: workoutComments){
-                    ProgressCommentDTO workoutCommentDTO = new ProgressCommentDTO();
-                    workoutCommentDTO.setId(workoutComment.getId());
-                    workoutCommentDTO.setText(workoutComment.getText());
-                    workoutCommentDTO.setWorkoutId(workoutComment.getWorkoutId());
-                    workoutCommentDTO.setCreatedAt(workoutComment.getCreatedAt());
-                    workoutCommentDTO.setUpdatedAt(workoutComment.getUpdatedAt());
-                    workoutCommentDTO.setUserId(workoutComment.getUserId());
-                    Optional<User> commentedUser =  userRepository.findById(workoutComment.getUserId());
+                for(ProgressComment progressComment: progressComments){
+                    ProgressCommentDTO progressCommentDTO = new ProgressCommentDTO();
+                    progressCommentDTO.setId(progressComment.getId());
+                    progressCommentDTO.setText(progressComment.getText());
+                    progressCommentDTO.setProgressId(progressComment.getProgressId());
+                    progressCommentDTO.setCreatedAt(progressComment.getCreatedAt());
+                    progressCommentDTO.setUpdatedAt(progressComment.getUpdatedAt());
+                    progressCommentDTO.setUserId(progressComment.getUserId());
+                    Optional<User> commentedUser =  userRepository.findById(progressComment.getUserId());
                     if(commentedUser.isPresent()) {
-                        workoutCommentDTO.setUsername(commentedUser.get().getUsername());
-                        workoutCommentDTO.setProfileImage(commentedUser.get().getProfileImage());
+                        progressCommentDTO.setUsername(commentedUser.get().getUsername());
+                        progressCommentDTO.setProfileImage(commentedUser.get().getProfileImage());
                     }
                     if(commentedUser.isPresent()) {
-                        workoutCommentDTOList.add(workoutCommentDTO);
-                    }
-
-                }
-
-                workoutDTO.setWorkoutComments(workoutCommentDTOList);
-            }
-            if(user.isPresent()) {
-                workoutDTOList.add(workoutDTO);
-            }
-
-        }
-
-        return new ResponseEntity<List<ProgressDTO>>(workoutDTOList, HttpStatus.OK);
-    }
-
-    public ResponseEntity<?> getWorkoutsByUserId(String userId) {
-        List<Progress> workouts = workoutRepository.findByUserId(userId);
-        List<ProgressDTO> workoutDTOList = new ArrayList<>();
-
-        for (Progress workout:workouts) {
-            ProgressDTO workoutDTO = new ProgressDTO();
-            workoutDTO.setId(workout.getId());
-            workoutDTO.setCaption(workout.getCaption());
-            workoutDTO.setImgLink(workout.getImgLink());
-            workoutDTO.setUpdatedAt(workout.getUpdatedAt());
-            workoutDTO.setCreatedAt(workout.getCreatedAt());
-            workoutDTO.setLikedby(workout.getLikedby());
-            workoutDTO.setUserId(workout.getUserId());
-
-            Optional<User> user =  userRepository.findById(workout.getUserId());
-            if(user.isPresent()) {
-                workoutDTO.setUsername(user.get().getUsername());
-                workoutDTO.setProfileImage(user.get().getProfileImage());
-            }
-
-            List<ProgressComment> workoutComments = workoutCommentRepository.findByWorkoutId(workout.getId());
-            if(workoutComments.size() > 0){
-                List<ProgressCommentDTO> workoutCommentDTOList = new ArrayList<>();
-
-                for(ProgressComment workoutComment: workoutComments){
-                    ProgressCommentDTO workoutCommentDTO = new ProgressCommentDTO();
-                    workoutCommentDTO.setId(workoutComment.getId());
-                    workoutCommentDTO.setText(workoutComment.getText());
-                    workoutCommentDTO.setWorkoutId(workoutComment.getWorkoutId());
-                    workoutCommentDTO.setCreatedAt(workoutComment.getCreatedAt());
-                    workoutCommentDTO.setUpdatedAt(workoutComment.getUpdatedAt());
-                    workoutCommentDTO.setUserId(workoutComment.getUserId());
-                    Optional<User> commentedUser =  userRepository.findById(workoutComment.getUserId());
-                    if(commentedUser.isPresent()) {
-                        workoutCommentDTO.setUsername(commentedUser.get().getUsername());
-                        workoutCommentDTO.setProfileImage(commentedUser.get().getProfileImage());
-                    }
-                    if(commentedUser.isPresent()) {
-                        workoutCommentDTOList.add(workoutCommentDTO);
+                        progressCommentDTOList.add(progressCommentDTO);
                     }
 
                 }
 
-                workoutDTO.setWorkoutComments(workoutCommentDTOList);
+                progressDTO.setProgressComments(progressCommentDTOList);
             }
             if(user.isPresent()) {
-                workoutDTOList.add(workoutDTO);
+                progressDTOList.add(progressDTO);
             }
 
         }
 
-        return new ResponseEntity<List<ProgressDTO>>(workoutDTOList, HttpStatus.OK);
+        return new ResponseEntity<List<ProgressDTO>>(progressDTOList, HttpStatus.OK);
     }
-    public ResponseEntity<?> saveWorkout(Progress workoutShare){
+
+    public ResponseEntity<?> getProgressesByUserId(String userId) {
+        List<Progress> progresses = progressRepository.findByUserId(userId);
+        List<ProgressDTO> progressDTOList = new ArrayList<>();
+
+        for (Progress progress:progresses) {
+            ProgressDTO progressDTO = new ProgressDTO();
+            progressDTO.setId(progress.getId());
+            progressDTO.setCaption(progress.getCaption());
+            progressDTO.setImgLink(progress.getImgLink());
+            progressDTO.setUpdatedAt(progress.getUpdatedAt());
+            progressDTO.setCreatedAt(progress.getCreatedAt());
+            progressDTO.setLikedby(progress.getLikedby());
+            progressDTO.setUserId(progress.getUserId());
+
+            Optional<User> user =  userRepository.findById(progress.getUserId());
+            if(user.isPresent()) {
+                progressDTO.setUsername(user.get().getUsername());
+                progressDTO.setProfileImage(user.get().getProfileImage());
+            }
+
+            List<ProgressComment> progressComments = progressCommentRepository.findByProgressId(progress.getId());
+            if(progressComments.size() > 0){
+                List<ProgressCommentDTO> progressCommentDTOList = new ArrayList<>();
+
+                for(ProgressComment progressComment: progressComments){
+                    ProgressCommentDTO progressCommentDTO = new ProgressCommentDTO();
+                    progressCommentDTO.setId(progressComment.getId());
+                    progressCommentDTO.setText(progressComment.getText());
+                    progressCommentDTO.setProgressId(progressComment.getProgressId());
+                    progressCommentDTO.setCreatedAt(progressComment.getCreatedAt());
+                    progressCommentDTO.setUpdatedAt(progressComment.getUpdatedAt());
+                    progressCommentDTO.setUserId(progressComment.getUserId());
+                    Optional<User> commentedUser =  userRepository.findById(progressComment.getUserId());
+                    if(commentedUser.isPresent()) {
+                        progressCommentDTO.setUsername(commentedUser.get().getUsername());
+                        progressCommentDTO.setProfileImage(commentedUser.get().getProfileImage());
+                    }
+                    if(commentedUser.isPresent()) {
+                        progressCommentDTOList.add(progressCommentDTO);
+                    }
+
+                }
+
+                progressDTO.setProgressComments(progressCommentDTOList);
+            }
+            if(user.isPresent()) {
+                progressDTOList.add(progressDTO);
+            }
+
+        }
+
+        return new ResponseEntity<List<ProgressDTO>>(progressDTOList, HttpStatus.OK);
+    }
+    public ResponseEntity<?> saveProgress(Progress progressShare){
         try{
-            workoutShare.setCreatedAt(new Date(System.currentTimeMillis()));
-            workoutShare.setUpdatedAt(new Date(System.currentTimeMillis()));
-            workoutRepository.save(workoutShare);
-            return new ResponseEntity<Progress>(workoutShare, HttpStatus.OK);
+            progressShare.setCreatedAt(new Date(System.currentTimeMillis()));
+            progressShare.setUpdatedAt(new Date(System.currentTimeMillis()));
+            progressRepository.save(progressShare);
+            return new ResponseEntity<Progress>(progressShare, HttpStatus.OK);
         }catch(Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    public ResponseEntity<?> updateWorkoutById(String id,Progress workout){
-        Optional<Progress> existingWorkout =  workoutRepository.findById(id);
-        if(existingWorkout.isPresent()){
-            Progress updateWorkout = existingWorkout.get();
-            if(workout.getCaption() != null) {
-                updateWorkout.setCaption(workout.getCaption());
+    public ResponseEntity<?> updateProgressById(String id,Progress progress){
+        Optional<Progress> existingProgress =  progressRepository.findById(id);
+        if(existingProgress.isPresent()){
+            Progress updateProgress = existingProgress.get();
+            if(progress.getCaption() != null) {
+                updateProgress.setCaption(progress.getCaption());
             }
-            if(workout.getImgLink() != null) {
-                updateWorkout.setImgLink(workout.getImgLink());
+            if(progress.getImgLink() != null) {
+                updateProgress.setImgLink(progress.getImgLink());
             }
-            updateWorkout.setUpdatedAt(new Date(System.currentTimeMillis()));
-            return new ResponseEntity<>(workoutRepository.save(updateWorkout), HttpStatus.OK);
+            updateProgress.setUpdatedAt(new Date(System.currentTimeMillis()));
+            return new ResponseEntity<>(progressRepository.save(updateProgress), HttpStatus.OK);
         }else{
             return new ResponseEntity<>("Progress Update Error",HttpStatus.NOT_FOUND);
         }
     }
-    public ResponseEntity<?> likeWorkoutById(String id,Progress workout){
-        Optional<Progress> existingWorkout =  workoutRepository.findById(id);
-        if(existingWorkout.isPresent()){
-            Progress updateWorkout = existingWorkout.get();
-            if(workout.getLikedby() != null) {
-                updateWorkout.setLikedby(workout.getLikedby());
+    public ResponseEntity<?> likeProgressById(String id,Progress progress){
+        Optional<Progress> existingProgress =  progressRepository.findById(id);
+        if(existingProgress.isPresent()){
+            Progress updateProgress = existingProgress.get();
+            if(progress.getLikedby() != null) {
+                updateProgress.setLikedby(progress.getLikedby());
             }
-            return new ResponseEntity<>(workoutRepository.save(updateWorkout), HttpStatus.OK);
+            return new ResponseEntity<>(progressRepository.save(updateProgress), HttpStatus.OK);
         }else{
             return new ResponseEntity<>("Progress Update Error",HttpStatus.NOT_FOUND);
         }
     }
-    public ResponseEntity<?> deleteWorkoutById(String id){
+    public ResponseEntity<?> deleteProgressById(String id){
         try{
-            workoutRepository.deleteById(id);
+            progressRepository.deleteById(id);
             return new ResponseEntity<>("Success deleted with " + id,HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
